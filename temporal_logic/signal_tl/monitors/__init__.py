3b4f33b942a70d0587614b63424ad8f78376d3e2
Anand Balakrishnan <anandbala1597@gmail.com>
2019-02-14 16:40:59 -0800

    Remove old semantics folder

--- temporal_logic/signal_tl/monitors/__init__.py
+++ temporal_logic/signal_tl/monitors/__init__.py
@@ -1,6 +1,10 @@
-from .efficient_robustness import efficient_robustness
-import temporal_logic.signal_tl as stl
 import pandas as pd
+
+import temporal_logic.signal_tl as stl
+
+from .efficient_robustness import efficient_robustness
+from .lti_semantics import lti_filter_monitor
+
 
 def eval_bool(phi: stl.Expression, w: pd.DataFrame, t=None) -> pd.Series:
     rho = efficient_robustness(phi, w)
