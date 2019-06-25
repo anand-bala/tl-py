393513befd16fdf3444166c93b783023a5d00bb4
Anand Balakrishnan <anandbala1597@gmail.com>
2019-06-25 12:13:04 -0700

    Remove poetry dependency. Use setuptools

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+REQUIRED_PACKAGES = [
+    "sympy ~= 1.3",
+    "pandas ~= 0.24.1",
+    "numpy ~= 1.16",
+    "scipy ~= 1.2",
+]
+
+setup(
+    packages=find_packages(),
+    install_requires=REQUIRED_PACKAGES,
+    python_requires="~=3.3"
+)
